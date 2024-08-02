ad6918739d907ef4abfebb8a377a4c298a5a0357
analuizaestella <analuizafranciscoestella@gmail.com>
2024-08-02 19:30:31 -0300

    Script SQL Pets

---
+++ dbPets.sql
@@ -0,0 +1,8 @@
+-- apagar banco de dados
+drop database dbPets;
+-- criar o banco de dados 
+create database dbPets;
+-- acessar o banco de dados
+use dbPets;
+-- visualizando o banco de dados
+show databases;
